dcf8a2477dbc9cdae6c2c0602191aa8ee57aceeb
Liron <52330564+LironKesem@users.noreply.github.com>
2026-04-02 18:12:09 -0400

    changing the calling function to cutile kernel

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -850,8 +850,8 @@ def cutlass_scaled_mm(
     elif envs.VLLM_SWITCH_TO_CUTILE:
         logger.info(" VLLM_SWITCH_TO_CUTILE=True, using CuTile kernels")
         import vllm.kernels.cutile.cutile_w8a8
-        #out = torch.empty((a.shape[0], b.shape[1]), dtype=out_dtype, device=a.device)
-        out = torch.ops.vllm.cutile_scaled_mm(a, b, scale_a, scale_b, out_dtype) # add out as input to the kernel
+        out = torch.empty((a.shape[0], b.shape[1]), dtype=out_dtype, device=a.device)
+        torch.ops.vllm.cutile_scaled_mm_out(out, a, b, scale_a, scale_b, bias)
     else:
         out = torch.empty((a.shape[0], b.shape[1]), dtype=out_dtype, device=a.device)
         torch.ops._C.cutlass_scaled_mm(out, a, b, scale_a, scale_b, bias)
